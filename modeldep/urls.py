3c7c3769a2c9b725e842d53d53c313027ad064c8
Abhisht01 <88162021+Abhisht01@users.noreply.github.com>
2021-12-18 00:17:43 +0530

    for hosting

--- modeldep/urls.py
+++ modeldep/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from modeldep import views
+from django.views.static import serve
+from django,core.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('resultt/',views.resultt,name='resultt'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
